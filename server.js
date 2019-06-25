89dcd69221c607e3094750eb03f279f6aec817b1
nyapal <nyapal14@gmail.com>
2019-06-25 11:23:36 -0700

    added handlebars

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.engine('handlebars', exphbs())
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+// app.get('/', (req, res) {
+//   res.render('home')
+// })
+  
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
